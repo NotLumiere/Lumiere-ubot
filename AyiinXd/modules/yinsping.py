0e08aebe8e74286aee7d1b8ae28f3fa477b191ee
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-04-17 12:52:57 +0700

    Update yinsping.py

--- AyiinXd/modules/yinsping.py
+++ AyiinXd/modules/yinsping.py
@@ -78,8 +78,8 @@ async def ayiin(ganteng):
 
 CMD_HELP.update(
     {
-        "yinsping": f"**Plugin:** `yinsping`\
-        \n\n  »  **Perintah : **`Perintah Ini Hanya Untuk Devs Ayiin-Userbot Tod.`\
+        "Cping": f"**Plugin:** `Cping`\
+        \n\n  »  **Perintah : **`Perintah Ini Hanya Untuk Devs Lumiere-ubot Tod.`\
         \n  »  **Kegunaan :** __Silahkan Ketik `{cmd}ping` Untuk Publik.__\
     "
     }
