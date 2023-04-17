3392308dc4fe74bb13f470130f0b306594f796de
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-04-17 15:08:19 +0700

    Update helper.py

--- AyiinXd/modules/helper.py
+++ AyiinXd/modules/helper.py
@@ -22,13 +22,13 @@ CMD_HELP.update(
     {
         "helper": f"**Plugin : **`helper`\
         \n\n  »  **Perintah :** `{cmd}ihelp`\
-        \n  »  **Kegunaan : **Bantuan Untuk Ayiin-Userbot.\
+        \n  »  **Kegunaan : **Bantuan Untuk Lumiere-ubot.\
         \n\n  »  **Perintah :** `{cmd}listvar`\
         \n  »  **Kegunaan : **Melihat Daftar Vars.\
         \n\n  »  **Perintah :** `{cmd}repo`\
-        \n  »  **Kegunaan : **Melihat Repository Ayiin-Userbot.\
+        \n  »  **Kegunaan : **Melihat Repository Lumiere-ubot.\
         \n\n  »  **Perintah :** `{cmd}string`\
-        \n  »  **Kegunaan : **Link untuk mengambil String Ayiin-Userbot.\
+        \n  »  **Kegunaan : **Link untuk mengambil String Lumiere-ubot.\
     "
     }
 )
