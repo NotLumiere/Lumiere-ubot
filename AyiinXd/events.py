a445dc2900ef35945ffd2930c3250c8d2a025937
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-04-17 12:18:00 +0700

    Update events.py

--- AyiinXd/events.py
+++ AyiinXd/events.py
@@ -200,8 +200,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ AYIIN-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/AyiinXdSupport)"
+                    text = "**✘ LUMIERE-UBOT ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/LumiereSupport)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -210,7 +210,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @AyiinXdSupport"
+                    ftext += "\ncukup forward saja pesan ini ke @Lumiere-ubot"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
